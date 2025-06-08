5671cd10c53d22a3fb52f8c676fe97a624baa41b
Побединский Давид <pda.private@yandex.ru>
2025-06-08 16:01:09 +0300

    Фикс проблем с появлением модальных окон

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import {
 	cardImagePopup,
 	imagePopupImageCaption,
 	imagePopupImage,
-	$popupEditProfile,
-	$popups,
+	popupEditProfile,
+	popups,
 	profileImagePopup,
 } from './components/modal/constants';
 import { cardsList } from './components/card/constants';
@@ -23,38 +23,38 @@ let currentUserId = '';
 //#endregion
 
 //#region Popup common handlers
-$popups.forEach((popup) => {
+popups.forEach((popup) => {
 	popup.addEventListener('mousedown', (e) => handleClickOutsideClose(e, popup));
 	popup.querySelector('.popup__close').addEventListener('click', () => handleCloseModal(popup));
 });
 //#endregion
 
 //#region Profile DOM nodes
-const $profileTitle = document.querySelector('.profile__title');
-const $profileDescription = document.querySelector('.profile__description');
-const $profileImage = document.querySelector('.profile__image');
-const $profileAvatarBtn = document.querySelector('.profile__image-cover');
-const $profileEditBtn = document.querySelector('.profile__edit-button');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const profileImage = document.querySelector('.profile__image');
+const profileAvatarBtn = document.querySelector('.profile__image-cover');
+const profileEditBtn = document.querySelector('.profile__edit-button');
 //#endregion
 
 //#region Profile avatar form
-const $avatarForm = profileImagePopup.querySelector('.popup__form[name="edit-avatar"]');
-const $avatarInput = $avatarForm.querySelector('.popup__input_type_url');
+const avatarForm = profileImagePopup.querySelector('.popup__form[name="edit-avatar"]');
+const avatarInput = avatarForm.querySelector('.popup__input_type_url');
 
-$profileAvatarBtn.addEventListener('click', () => {
-	$avatarForm.reset();
-	clearValidation($avatarForm, validationConfig);
+profileAvatarBtn.addEventListener('click', () => {
+	avatarForm.reset();
+	clearValidation(avatarForm, validationConfig);
 	handleOpenModal(profileImagePopup);
 });
 
-$avatarForm.addEventListener('submit', async (e) => {
+avatarForm.addEventListener('submit', async (e) => {
 	e.preventDefault();
 	const submitBtn = profileImagePopup.querySelector(validationConfig.submitButtonSelector);
 
 	try {
 		toggleLoading(true, submitBtn);
-		const { avatar } = await updateAvatar($avatarInput.value);
-		$profileImage.style.backgroundImage = `url('${avatar}')`;
+		const { avatar } = await updateAvatar(avatarInput.value);
+		profileImage.style.backgroundImage = `url('${avatar}')`;
 		handleCloseModal(profileImagePopup);
 	} catch (err) {
 		console.error(err);
@@ -65,27 +65,27 @@ $avatarForm.addEventListener('submit', async (e) => {
 //#endregion
 
 //#region Profile main form
-const $profileForm = $popupEditProfile.querySelector('.popup__form[name="edit-profile"]');
-const $nameInput = $profileForm.querySelector('.popup__input_type_name');
-const $jobInput = $profileForm.querySelector('.popup__input_type_description');
+const profileForm = popupEditProfile.querySelector('.popup__form[name="edit-profile"]');
+const nameInput = profileForm.querySelector('.popup__input_type_name');
+const jobInput = profileForm.querySelector('.popup__input_type_description');
 
-$profileEditBtn.addEventListener('click', () => {
-	clearValidation($profileForm, validationConfig);
-	$nameInput.value = $profileTitle.textContent;
-	$jobInput.value = $profileDescription.textContent;
-	handleOpenModal($popupEditProfile);
+profileEditBtn.addEventListener('click', () => {
+	clearValidation(profileForm, validationConfig);
+	nameInput.value = profileTitle.textContent;
+	jobInput.value = profileDescription.textContent;
+	handleOpenModal(popupEditProfile);
 });
 
-$profileForm.addEventListener('submit', async (e) => {
+profileForm.addEventListener('submit', async (e) => {
 	e.preventDefault();
-	const submitBtn = $popupEditProfile.querySelector(validationConfig.submitButtonSelector);
+	const submitBtn = popupEditProfile.querySelector(validationConfig.submitButtonSelector);
 
 	try {
 		toggleLoading(true, submitBtn);
-		const { name, about } = await editProfile($nameInput.value, $jobInput.value);
-		$profileTitle.textContent = name;
-		$profileDescription.textContent = about;
-		handleCloseModal($popupEditProfile);
+		const { name, about } = await editProfile(nameInput.value, jobInput.value);
+		profileTitle.textContent = name;
+		profileDescription.textContent = about;
+		handleCloseModal(popupEditProfile);
 	} catch (err) {
 		console.error(err);
 	} finally {
@@ -95,24 +95,24 @@ $profileForm.addEventListener('submit', async (e) => {
 //#endregion
 
 //#region Card add form
-const $addCardBtn = document.querySelector('.profile__add-button');
-const $addCardForm = addCardPopup.querySelector('.popup__form');
-const $cardNameIn = $addCardForm.querySelector('.popup__input_type_card-name');
-const $cardLinkIn = $addCardForm.querySelector('.popup__input_type_url');
+const addCardBtn = document.querySelector('.profile__add-button');
+const addCardForm = addCardPopup.querySelector('.popup__form');
+const cardNameIn = addCardForm.querySelector('.popup__input_type_card-name');
+const cardLinkIn = addCardForm.querySelector('.popup__input_type_url');
 
-$addCardBtn.addEventListener('click', () => {
-	$addCardForm.reset();
-	clearValidation($addCardForm, validationConfig);
+addCardBtn.addEventListener('click', () => {
+	addCardForm.reset();
+	clearValidation(addCardForm, validationConfig);
 	handleOpenModal(addCardPopup);
 });
 
-$addCardForm.addEventListener('submit', async (e) => {
+addCardForm.addEventListener('submit', async (e) => {
 	e.preventDefault();
 	const submitBtn = addCardPopup.querySelector(validationConfig.submitButtonSelector);
 
 	try {
 		toggleLoading(true, submitBtn);
-		const cardData = await postCard($cardNameIn.value, $cardLinkIn.value);
+		const cardData = await postCard(cardNameIn.value, cardLinkIn.value);
 		prependCard(createCard(cardData, currentUserId, removeCard, likeCard, () => openImageModal(cardData.link, cardData.name)));
 		handleCloseModal(addCardPopup);
 	} catch (err) {
@@ -143,9 +143,9 @@ export const appendCard = (card) => cardsList.append(card);
 		const [user, cards] = await getInitialInfo();
 		currentUserId = user._id;
 
-		$profileTitle.textContent = user.name;
-		$profileDescription.textContent = user.about;
-		$profileImage.style.backgroundImage = `url('${user.avatar}')`;
+		profileTitle.textContent = user.name;
+		profileDescription.textContent = user.about;
+		profileImage.style.backgroundImage = `url('${user.avatar}')`;
 
 		cards.forEach((c) => appendCard(createCard(c, currentUserId, removeCard, likeCard, () => openImageModal(c.link, c.name))));
 	} catch (err) {
